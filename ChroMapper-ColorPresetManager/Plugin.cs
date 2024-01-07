6b3a6f3f1bffd2817d583e13c947e2bf7d557305
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2024-01-07 11:01:01 +0900

    プロジェクトファイルを追加します

---
+++ ChroMapper-ColorPresetManager/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ChroMapper_ColorPresetManager
+{
+    [Plugin("ChroMapper-ColorPresetManager")]
+    public class Plugin
+    {
+        [Init]
+        private void Init()
+        {
+            Debug.Log("ChroMapper-ColorPresetManager Plugin has loaded!");
+        }
+        [Exit]
+        private void Exit()
+        {
+            Debug.Log("ChroMapper-ColorPresetManager Plugin has closed!");
+        }
+    }
+}
